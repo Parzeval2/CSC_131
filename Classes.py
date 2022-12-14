63bacd778041062c635078d7925d6afa18858c25
Grant <coopergrant4@gmail.com>
2022-12-14 10:03:33 -0600

    Classes in class

--- Classes.py
+++ Classes.py
@@ -5,10 +5,26 @@ class Point:
     @property
     def x(self):
         return self._x
+    @property
+    def y(self):
+        return self._y
 
     @x.setter
-    def x(self, value):
-        self._x = value
-
+    def x(self, x):
+        if (x < 10):
+            self._x = -10
+        elif (x > 10):
+            self._x = 10
+        else:
+            self._x = x
+    @y.setter
+    def y(self, y):
+        if (y < 10):
+            self._y = -10
+        elif (y > 10):
+            self._y = 10
+        else:
+            self._y = y
 p1 = Point()
 print("p1=({}, {})".format(p1.x, p1.y))
+p2 = Point(-50,50)
