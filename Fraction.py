6618a6ad861938962b0a4dad3510f461a06b8ff8
Grant <coopergrant4@gmail.com>
2022-12-16 09:59:29 -0600

    Added some extra bits to the fraction class

--- Fraction.py
+++ Fraction.py
@@ -34,6 +34,21 @@ class Fraction:
         if (value != 0):
             self._den = value
 
+    def reduce(self):
+        gcd = 1
+        minimum = min(abs(self.num), abs(self.den))
+
+        #find common divisors
+        for i in range(2, int(minimum + 1)):
+            if self.num % i == 0 and self.den % i == 0:
+                gcd = i
+
+        #divide numerator and denominator by the GCD
+        self.num /= gcd
+        self.den /= gcd
+        #if the numerator is 0, set the denominator to 1
+        if (self.num == 0):
+            self.den = 1
     def get_real(self):
         return float(self.num) / self.den
     def __str__(self):
