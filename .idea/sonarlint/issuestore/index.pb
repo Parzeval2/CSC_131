7832a0e9a310eecb15af9b0f13b111ee0cc11345
Grant Cooper <69401337+Parzeval2@users.noreply.github.com>
2023-02-01 09:57:45 -0600

    bruv

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,12 @@
 Fraction.py,a\3\a3722386a9fd49253de0707bebabe8d7bacac4f0
 <
 Room Game.py,7\2\722b3379ae2a02a0b01026297a7aeee48ed95b50
+:
+
+Patient.py,1\5\153e320c515c3dff630688c56f5b620addc20dd1
+>
+PatientTest.py,7\6\76364a35c7a8c6539903ff8ddb7cc3f800fe45a5
+6
+GUi.py,4\d\4dc985a564ef8dfee83e05b207e0209269f06cdc
+9
+	Adders.py,5\a\5a781a15e80aa150310ac50ba5d9a2fb902ff06b
--- GUi.py
+++ GUi.py
@@ -14,10 +14,26 @@ class GUITest(Frame):
         Label3 = Label(self.master, text="A third label, centered")
         Label3.grid(row=2, column=0, columnspan=2, sticky=E+W)
 
-        img = PhotoImage(file="Hair needs cut.png")
-        Label4 = Label(self.master, image=img)
-        Label4.image = img
-        Label4.grid(row=0, column=2, columnspan=2, rowspan=2, sticky=N+S+E+W)
+        # img = PhotoImage(file="Hair needs cut.png")
+        # Label4 = Label(self.master, image=img)
+        # Label4.image = img
+        # Label4.grid(row=0, column=2, columnspan=2, rowspan=2, sticky=N+S+E+W)
+
+        entry1 = Entry(self.master)
+        entry1.grid(row=0, column=1)
+
+        entry2 = Entry(self.master)
+        entry2.insert(END, "user input")
+        entry2.grid(row=1, column=1)
+
+        checkbutton1 = Checkbutton(self.master, text="some checkbutton option")
+        checkbutton1.grid(row=3, column=0, columnspan=2, sticky=W)
+
+        button1 = Button(self.master, text="A button")
+        button1.grid(row=3, column=2)
+
+        button2 = Button(self.master, text="another button")
+        button2.grid(row=3, column=3)
 
 window = Tk()
 t = GUITest(window)
